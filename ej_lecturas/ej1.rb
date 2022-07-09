434813a494f395ec9b294ce437a10426334161b3
MariaJoseWelkner <josewelkner@gmail.com>
2022-07-08 23:50:53 -0400

    agregando modulo8

---
+++ ej_lecturas/ej1.rb
@@ -0,0 +1,7 @@
+# creando nuestra primera clase
+class MiPrimeraClase #nombres de las clases siempre empiezan con mayuscula
+end
+# una vez creada la clase, se puede instanciar
+# una vez definida la clase, se puede usar para crear todos los objetos que se necesiten
+a = MiPrimeraClase.new #MiPrimeraClase es el objeto
+print a.class #me imprime el objeto MiPrimeraClase
